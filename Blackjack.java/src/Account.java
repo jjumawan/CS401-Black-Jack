2bc4fb0d157c628e2efd13761510a7c74b0de0b2
Jievy Lance Jumawan <jjumawan@horizon.csueastbay.edu>
2023-04-24 16:18:25 -0700

    initial Account.java

---
+++ Blackjack.java/src/Account.java
@@ -0,0 +1,61 @@
+
+public class Account {
+    private UserLogin userIDPassword;
+    private int balance;
+    private AccountStatus accountStatus;
+    
+    public Account() {
+        this.userIDPassword = null;
+        this.balance = 0;
+        this.accountStatus = AccountStatus.OFFLINE;
+    }
+    
+    public void login(UserLogin user) {
+        if (user.getUsername().equals(this.userIDPassword.getUsername()) && user.getPassword().equals(this.userIDPassword.getPassword())) {
+            this.accountStatus = AccountStatus.ONLINE;
+            System.out.println("Login successful.");
+        } else {
+            System.out.println("Invalid username or password.");
+        }
+    }
+    
+    public void signUp(UserLogin user) {
+        this.userIDPassword = user;
+        this.accountStatus = AccountStatus.ONLINE;
+        System.out.println("Account created.");
+    }
+    
+    public void logOut() {
+        this.accountStatus = AccountStatus.OFFLINE;
+        System.out.println("Logged out.");
+    }
+    
+    public String getUserID() {
+        return this.userIDPassword.getUsername();
+    }
+    
+    public void addBalance(int amount) {
+        this.balance += amount;
+        System.out.println(amount + " added to balance. New balance: " + this.balance);
+    }
+    
+    public void withdrawBalance(int amount) {
+        if (amount > this.balance) {
+            System.out.println("Insufficient funds.");
+        } else {
+            this.balance -= amount;
+            System.out.println(amount + " withdrawn from balance. New balance: " + this.balance);
+        }
+    }
+    
+    public void updateAccountStatus() {
+    	if (this.balance < 0) {
+            this.accountStatus = AccountStatus.OFFLINE;
+            System.out.println("Account status updated to OFFLINE due to negative balance.");
+        }
+    }
+    
+    public AccountStatus getAccountStatus() {
+        return this.accountStatus;
+    }
+}
